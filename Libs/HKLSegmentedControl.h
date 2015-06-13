c3a74c681dbbc742728cfb611ed871639e91f5e5
Hirohito Kato <hkato193@gmail.com>
2015-06-13 14:05:19 +0900

    Add API reference

--- Libs/HKLSegmentedControl.h
+++ Libs/HKLSegmentedControl.h
@@ -9,13 +9,34 @@
 
 @protocol HKLSegmentedControlDelegate;
 
+/**
+ * HKLSegmentedControl class is the subclass of <code>UISegmentedControl</code>.
+ * In addition to full feature of <code>UISegmentedControl</code>, it can fire
+ * <b>dynamic value changed</b> event.
+ */
 @interface HKLSegmentedControl : UISegmentedControl
+
+/** The object that acts as the delegate of the segmented control. */
 @property (nonatomic, weak)id<HKLSegmentedControlDelegate> delegate;
+
+/**
+ * Current highlighted segment index you touch. If you don't touch it,
+ * The value is same with that of <code>selectedSegmentIndex</code> property.
+ */
 @property (nonatomic, readonly)NSInteger highlightedSegmentIndex;
 @end
 
 @protocol HKLSegmentedControlDelegate <NSObject>
 @optional
+/**
+ * Called when the touched segment is changed.
+ *
+ * This method is only called when you are touching the segmented control.
+ * It is not called if you touch up it. Use default "Value Changed" event to handle it.
+ *
+ * @param control                 The HKLSegmentedControl object you touched.
+ * @param highlightedSegmentIndex Current segment index you touched.
+ */
 - (void)  segmentedControl:(HKLSegmentedControl*)control
 didChangedHighlightedIndex:(NSInteger)highlightedSegmentIndex;
 @end
